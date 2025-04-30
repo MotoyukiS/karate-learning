95bfaad97ac9b2ae1ca6f3a4cee155d1c21d0dde
MotoyukiS <motoyuki.suzuki@live.com>
2025-04-30 22:32:02 +0000

    Moved test class and reconfigure

--- demo/src/test/java/com/example/CalculatorTest.java
+++ demo/src/test/java/com/example/CalculatorTest.java
@@ -1,8 +1,7 @@
+package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-
-import com.example.Calculator;
 
 public class CalculatorTest {
     private final Calculator calculator = new Calculator();
